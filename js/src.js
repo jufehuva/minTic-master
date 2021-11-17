072f98f34e3575d1f04168aa2c47fa520562ef63
jufehuva <juanf.hurtadov@gmail.com>
2021-11-16 22:16:11 -0500

    quinto

---
+++ js/src.js
@@ -0,0 +1,10 @@
+let operaciones=[];
+let balance=0;
+
+const registrarOperacion=(monto,tipo,categoria,fecha)=>{}
+const registrarMetaAhorro=(meta)=>{}
+const estaMiMetaCumplida=()=>{}
+
+module.exports.registrarMetaAhorro=registrarMetaAhorro;
+module.exports.estaMiMetaCumplida=estaMiMetaCumplida;
+module.exports.registrarOperacion=registrarOperacion;
